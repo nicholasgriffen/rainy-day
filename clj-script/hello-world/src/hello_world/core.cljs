d1417e7310a2cf86878ca6897d939d61056d4650
Nick Griffen <nicholas.s.griffen@gmail.com>
2018-09-17 09:34:39 -0600

    functioning event listeners, vanilla JS DOM element retrieval and fetch

--- clj-script/hello-world/src/hello_world/core.cljs
+++ clj-script/hello-world/src/hello_world/core.cljs
@@ -1,16 +1,32 @@
 (ns hello-world.core)
+
 (println :started)
 
-;; get editor
-;; get stuff inside of editor
+(def api "https://api.github.com/users/nicholasgriffen")
+
+(defn getDOMElement
+   "Get DOM element by id [id]"
+   [id]
+   (.getElementById js/document (str id)))
 
 (defn getText
-  "Get inner text of DOM element with ID [id]"
-  [id]
-  (let [elem (.getElementById js/document (str id))] elem.innerText))
+   "Get inner text of DOM element"
+   [id]
+   (let [elem (getDOMElement id)], elem.innerText))
 
-;; do something like println that stuff
-(println (getText "editor"))
+(defn listenFor
+   "Listen for event on DOM element and do callback"
+   [event id callback]
+   (.addEventListener (getDOMElement id) event callback))
 
+;; ugly but demonstrative example of using browser fetch api in clojurescript
+;; js ;; fetch('url').then(res => res.json()).then(res => console.log(res))
+;; cljs ;; (.then (.then (.fetch js/window "https://api.github.com/users/nicholasgriffen"))
+;   (fn [res] (.json res)) (fn [res] (println res)))
+(.catch (.then (.then (.fetch js/window api) #(.json %)) #(println %)) #(println %))
+; (.. (.fetch js/window "https://api.github.com/us/nicholasgriffen")
+;     (then #(.json %))
+;     (then println))
 ;; add event listener to editor and log inner text on click
-(.addEventListener (.getElementById js/document "editor") "click" (fn [e] (println (getText e.target.id))))
+(listenFor "click" "editor" (fn [event] (println (getText event.target.id))))
+(listenFor "submit" "github-form" (fn [event] (do (.preventDefault event) (.log js/console event.target))))
